b4e1717f2a1c123c2a0481715b6f8cca08700453
Miroslav Bondik <benya.senya.16@gmail.com>
2022-11-22 15:15:38 +0200

    Add files via upload
    added new method Update

--- script.js
+++ script.js
@@ -3,23 +3,46 @@ const btnUpdate = document.querySelector(".todolist__task-btn--update");
 const todoList = document.querySelector(".todolist");
 // const taskId = document.querySelector('.todolist__task-id')
 
+let dateInYYYYMMDD = new Date().toISOString().slice(0, 10);
+
 let user = [
   {
     description: "Обучение",
-    date: Date.now(),
+    date: dateInYYYYMMDD,
     id: 1,
   },
   {
-    description: "Обучение",
-    date: Date.now(),
+    description: "Курение",
+    date: dateInYYYYMMDD,
     id: 2,
   },
   {
-    description: "Обучение",
-    date: Date.now(),
+    description: "Отдых",
+    date: dateInYYYYMMDD,
     id: 3,
   },
 ];
+
+const initializeAllTodos = function () {
+    user.forEach((el) => {
+        const html = `
+                      <div class="todolist__task" data-index="${el.id}">
+                          <div class="todolist__task-text">
+                              <p class="todolist__task-desc">${el.description}</p>
+                              <span class="todolist__task-date">${el.date}</span>
+                              <span class="todolist__task-id">ID: ${el.id}</span>
+                          </div>
+                          <div class="todolist__task-btns">
+                              <button class="todolist__task-btn todolist__task-btn--delete" type="button">X</button>
+                              <button class="todolist__task-btn todolist__task-btn--update" type="button">update</button>
+                          </div>
+                      </div>
+                      `;
+    todoList.insertAdjacentHTML('afterbegin', html)
+    })
+};
+
+initializeAllTodos();
 
 const deleteTask = function (event) {
   if (!event.target.classList.contains("todolist__task-btn--delete")) return;
@@ -33,14 +56,34 @@ const deleteTask = function (event) {
   });
 
   user.splice(findIdxElementInObj, 1);
-  if(user.length == 0) {
-    todoList.style.display = 'none';
+  if (user.length == 0) {
+    todoList.style.display = "none";
   }
   // Remove from DOM
   event.target.closest(".todolist__task").remove();
   console.log(user);
 };
+const updateTask = function (event) {
+  if (!event.target.classList.contains("todolist__task-btn--update")) return;
+  let description = event.target
+    .closest("div")
+    .previousElementSibling.querySelector(".todolist__task-desc");
+  let taskId = event.target
+    .closest(".todolist__task")
+    .getAttribute("data-index");
+  let findUser = user.find((el) => el.id == taskId);
+  let updateTodo = prompt("Update your TODO", description.textContent);
+  findUser.description = updateTodo;
+  description.textContent = updateTodo;
+  console.log(user);
+};
 
-// const updateTask = function (event) {};
-
+// Delete
 todoList.addEventListener("click", deleteTask.bind(this));
+
+
+// Update
+todoList.addEventListener("click", updateTask.bind(this));
+
+
+
